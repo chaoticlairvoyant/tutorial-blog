d4a2919050b5aba5f2f7e873c41efc8f8141dcc5
claire <dirrclaire@gmail.com>
2024-09-09 11:12:30 +0200

    Fehlerbehandlung

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import preact from '@astrojs/preact';
 
 export default defineConfig({
+  integrations: [preact()],
   site: "https://tutorial-blog-claire.netlify.app"
 });
